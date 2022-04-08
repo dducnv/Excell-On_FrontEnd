c5aff262c19204d74ba17b7b3b9b3c2638da14ff
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-04-08 21:48:45 +0700

    update file

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 const login = () => {
     return (
         <>
-            <div class="h-screen bg-gradient-to-br from-blue-600 to-indigo-600 flex justify-center items-center w-full">
+            <div className="h-screen bg-gradient-to-br from-blue-600 to-indigo-600 flex justify-center items-center w-full">
                 <form>
-                    <div class="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
-                        <div class="space-y-4">
-                            <h1 class="text-center text-2xl font-semibold text-gray-900">Register</h1>
+                    <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
+                        <div className="space-y-4">
+                            <h1 className="text-center text-2xl font-semibold text-gray-900">Register</h1>
                             <div>
-                                <label for="email" class="block mb-1 text-gray-600 font-semibold">Username</label>
-                                <input type="text" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+                                <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">Username</label>
+                                <input type="text" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
                             </div>
                             <div>
-                                <label for="email" class="block mb-1 text-gray-600 font-semibold">Email</label>
-                                <input type="text" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+                                <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">Email</label>
+                                <input type="text" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
                             </div>
                             <div>
-                                <label for="email" class="block mb-1 text-gray-600 font-semibold">Password</label>
-                                <input type="text" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+                                <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">Password</label>
+                                <input type="text" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
                             </div>
                         </div>
-                        <button class="mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide">Register</button>
+                        <button className="mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide">Register</button>
                     </div>
                 </form>
             </div>
