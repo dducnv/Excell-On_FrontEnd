e3af9b416c1bf4b6bf73d0aaaa17ff29afe8cf26
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-04-11 22:00:03 +0700

    update file

--- pages/admin/dashboard.jsx
+++ pages/admin/dashboard.jsx
@@ -5,8 +5,7 @@ import Layout from '../../components/admin_layout/Layout';
 const dashboard = () => {
     return (
         <Layout>
-            <div className="px-6 pt-6 2xl:container">
-                
+            <div className="px-6 pt-6 2xl:container">     
                 <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
                     <div className="md:col-span-2 lg:col-span-1" >
                         <div className="h-full py-8 px-6 space-y-6 rounded-xl border border-gray-200 bg-white">
--- pages/services.jsx
+++ pages/services.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const services = () => {
+const services = () => {
   return (
     <div>services</div>
   )
 }
+
+export default services
