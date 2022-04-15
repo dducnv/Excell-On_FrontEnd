5695c2b92ce06f22f89c7a4f5c4a0fdc8d50aeb8
Quan-92 <71068237+Quan-92@users.noreply.github.com>
2022-04-15 14:32:02 +0700

    Contact-Order-Quan

--- pages/Order.jsx
+++ pages/Order.jsx
@@ -78,22 +78,22 @@ const Order = () => {
                                 <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
                                     <div className="flex justify-between  w-full">
                                         <p className="text-base leading-4 Text-black">Subtotal</p>
-                                        <p className="text-base leading-4 text-gray-600">$56.00</p>
+                                        <p className="text-base leading-4 text-gray-600 dark:text-gray-400">$56.00</p>
                                     </div>
                                     <div className="flex justify-between items-center w-full">
                                         <p className="text-base leading-4 Text-black">
                                             Discount <span className="bg-gray-200 dark:bg-gray-500 p-1 text-xs font-medium leading-3  Text-black dark:text-gray-300">STUDENT</span>
                                         </p>
-                                        <p className="text-base leading-4 text-gray-600">-$28.00 (50%)</p>
+                                        <p className="text-base leading-4 text-gray-600 dark:text-gray-400">-$28.00 (50%)</p>
                                     </div>
                                     <div className="flex justify-between items-center w-full">
                                         <p className="text-base leading-4 Text-black">Shipping</p>
-                                        <p className="text-base leading-4 text-gray-600">$8.00</p>
+                                        <p className="text-base leading-4 text-gray-600 dark:text-gray-400">$8.00</p>
                                     </div>
                                 </div>
                                 <div className="flex justify-between items-center w-full">
-                                    <p className="text-base font-semibold leading-4 Text-black">Total</p>
-                                    <p className="text-base font-semibold leading-4 text-gray-600">$36.00</p>
+                                    <p className="text-base font-semibold leading-4 Text-black ">Total</p>
+                                    <p className="text-base font-semibold leading-4 text-gray-600 dark:text-gray-400">$36.00</p>
                                 </div>
                             </div>
                             <div className="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
@@ -111,7 +111,7 @@ const Order = () => {
                                             </p>
                                         </div>
                                     </div>
-                                    <p className="text-lg font-semibold leading-6 Text-black">$8.00</p>
+                                    <p className="text-lg font-semibold leading-6 Text-black dark:text-gray-400">$8.00</p>
                                 </div>
                                 <div className="w-full flex justify-center items-center">
                                     <button className="hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-5 w-96 md:w-full bg-gray-800 dark:bg-gray-600 text-base font-medium leading-4 text-white dark:text-gray-400">View Carrier Details</button>
@@ -124,9 +124,9 @@ const Order = () => {
                         <div className="flex  flex-col md:flex-row xl:flex-col justify-start items-stretch h-full w-full md:space-x-6 lg:space-x-8 xl:space-x-0 ">
                             <div className="flex flex-col justify-start items-start flex-shrink-0">
                                 <div className="flex justify-center  w-full  md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
-                                    <img src="https://i.ibb.co/5TSg7f6/Rectangle-18.png" alt="avatar" />
+                                    <img className='w-10 h-10' src="https://dttc.site/wp-content/uploads/2021/03/CNDT-1-1.jpg" alt="avatar" />
                                     <div className=" flex justify-start items-start flex-col space-y-2">
-                                        <p className="text-base font-semibold leading-4 text-left Text-black">David Kent</p>
+                                        <p className="text-base font-semibold leading-4 text-left Text-black">BENJAMIN FRANKLIN</p>
                                         <p className="text-sm leading-5 text-gray-600">10 Previous Orders</p>
                                     </div>
                                 </div>
@@ -136,18 +136,18 @@ const Order = () => {
                                         <path d="M19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5Z" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
                                         <path d="M3 7L12 13L21 7" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
-                                    <p className="cursor-pointer text-sm leading-5 Text-black">david89@gmail.com</p>
+                                    <p className="cursor-pointer text-sm leading-5 Text-black">benjamin1706@gmail.com</p>
                                 </div>
                             </div>
                             <div className="flex justify-between xl:h-full  items-stretch w-full flex-col mt-6 md:mt-0">
                                 <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row  items-center md:items-start ">
                                     <div className="flex justify-center md:justify-start  items-center md:items-start flex-col space-y-4 xl:mt-8">
                                         <p className="text-base font-semibold leading-4 text-center md:text-left Text-black">Shipping Address</p>
-                                        <p className="w-48 lg:w-full xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">180 North King Street, Northhampton MA 1060</p>
+                                        <p className="w-48 lg:w-full xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600 dark:text-gray-400">180 North King Street, Northhampton MA 1060</p>
                                     </div>
                                     <div className="flex justify-center md:justify-start  items-center md:items-start flex-col space-y-4 ">
                                         <p className="text-base font-semibold leading-4 text-center md:text-left Text-black">Billing Address</p>
-                                        <p className="w-48 lg:w-full xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">180 North King Street, Northhampton MA 1060</p>
+                                        <p className="w-48 lg:w-full xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600 dark:text-gray-400">180 North King Street, Northhampton MA 1060</p>
                                     </div>
                                 </div>
                                 <div className="flex w-full justify-center items-center md:justify-start md:items-start">
