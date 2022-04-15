5a3e774560fb2390d419c4d28e0242d1090eb95b
Quan-92 <71068237+Quan-92@users.noreply.github.com>
2022-04-15 10:18:29 +0700

    Contact-Order-Quan

---
+++ pages/Order.jsx
@@ -0,0 +1,165 @@
+import React from 'react'
+import Layout from '../components/user_layout/Layout'
+
+const Order = () => {
+    return (
+        <Layout>
+            <div className="pt-24 pb-12 md:px-6 Text-black dark:text-gray-400 dark:bg-gray-700 2xl:px-20 2xl:container 2xl:mx-auto">
+                <div className="flex justify-start item-start space-y-2 flex-col ">
+                    <h1 className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9  Text-black dark:text-gray-400">Order service</h1>
+                    <p className="text-base font-medium leading-6 text-gray-600 dark:text-gray-400">Service online</p>
+                </div>
+                <div className="mt-10 flex flex-col  xl:flex-row jusitfy-center items-stretch  w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
+                    <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
+                        <div className="flex flex-col justify-start items-start bg-gray-100 dark:bg-gray-800 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
+                            <p className="text-lg dark:text-gray-400     md:text-xl font-semibold leading-6 xl:leading-5 Text-black">Customer’s Cart</p>
+                            <div className="mt-4 md:mt-6 flex  flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full ">
+                                <div className="pb-4 md:pb-8 w-full md:w-40">
+                                    <img className="w-full hidden md:block" src="https://i.ibb.co/84qQR4p/Rectangle-10.png" alt="dress" />
+                                    <img className="w-full md:hidden" src="https://i.ibb.co/L039qbN/Rectangle-10.png" alt="dress" />
+                                </div>
+                                <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full  pb-8 space-y-4 md:space-y-0">
+                                    <div className="w-full Text-black dark:text-gray-400 flex flex-col justify-start items-start space-y-8">
+                                        <h3 className="text-xl  xl:text-2xl font-semibold leading-6">Premium Quaility Dress</h3>
+                                        <div className="flex justify-start items-start flex-col space-y-2">
+                                            <p className="text-sm leading-none Text-black">
+                                                <span className="Text-black font-semibold">Style: </span> Italic Minimal Design
+                                            </p>
+                                            <p className="text-sm leading-none Text-black">
+                                                <span className="Text-black font-semibold">Size: </span> Small
+                                            </p>
+                                            <p className="text-sm leading-none Text-black">
+                                                <span className="Text-black font-semibold">Color: </span> Light Blue
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="flex justify-between space-x-8 items-start w-full">
+                                        <p className="text-base xl:text-lg leading-6">
+                                            $36.00 <span className="text-red-500 dark:text-red-300 line-through"> $45.00</span>
+                                        </p>
+                                        <p className="text-base xl:text-lg leading-6 Text-black">01</p>
+                                        <p className="text-base xl:text-lg font-semibold leading-6 Text-black">$36.00</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mt-6 md:mt-0 flex justify-start flex-col md:flex-row  items-start md:items-center space-y-4  md:space-x-6 xl:space-x-8 w-full ">
+                                <div className="w-full md:w-40">
+                                    <img className="w-full hidden md:block" src="https://i.ibb.co/s6snNx0/Rectangle-17.png" alt="dress" />
+                                    <img className="w-full md:hidden" src="https://i.ibb.co/BwYWJbJ/Rectangle-10.png" alt="dress" />
+                                </div>
+                                <div className="  flex justify-between items-start w-full flex-col md:flex-row space-y-4 md:space-y-0  ">
+                                    <div className="w-full flex flex-col justify-start items-start space-y-8">
+                                        <h3 className="text-xl xl:text-2xl font-semibold leading-6 Text-black">High Quaility Italic Dress</h3>
+                                        <div className="flex justify-start items-start flex-col space-y-2">
+                                            <p className="text-sm leading-none Text-black">
+                                                <span className="Text-black font-semibold">Style: </span> Italic Minimal Design
+                                            </p>
+                                            <p className="text-sm leading-none Text-black">
+                                                <span className="Text-black font-semibold">Size: </span> Small
+                                            </p>
+                                            <p className="text-sm leading-none Text-black">
+                                                <span className="Text-black font-semibold">Color: </span> Light Blue
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="flex justify-between space-x-8 items-start w-full">
+                                        <p className="text-base xl:text-lg leading-6">
+                                            $20.00 <span className="text-red-500 dark:text-red-300 line-through"> $30.00</span>
+                                        </p>
+                                        <p className="text-base xl:text-lg leading-6 Text-black">01</p>
+                                        <p className="text-base xl:text-lg font-semibold leading-6 Text-black">$20.00</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
+                            <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6   ">
+                                <h3 className="text-xl font-semibold leading-5 Text-black">Summary</h3>
+                                <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
+                                    <div className="flex justify-between  w-full">
+                                        <p className="text-base leading-4 Text-black">Subtotal</p>
+                                        <p className="text-base leading-4 text-gray-600">$56.00</p>
+                                    </div>
+                                    <div className="flex justify-between items-center w-full">
+                                        <p className="text-base leading-4 Text-black">
+                                            Discount <span className="bg-gray-200 dark:bg-gray-500 p-1 text-xs font-medium leading-3  Text-black dark:text-gray-300">STUDENT</span>
+                                        </p>
+                                        <p className="text-base leading-4 text-gray-600">-$28.00 (50%)</p>
+                                    </div>
+                                    <div className="flex justify-between items-center w-full">
+                                        <p className="text-base leading-4 Text-black">Shipping</p>
+                                        <p className="text-base leading-4 text-gray-600">$8.00</p>
+                                    </div>
+                                </div>
+                                <div className="flex justify-between items-center w-full">
+                                    <p className="text-base font-semibold leading-4 Text-black">Total</p>
+                                    <p className="text-base font-semibold leading-4 text-gray-600">$36.00</p>
+                                </div>
+                            </div>
+                            <div className="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
+                                <h3 className="text-xl font-semibold leading-5 Text-black">Shipping</h3>
+                                <div className="flex justify-between items-start w-full">
+                                    <div className="flex justify-center items-center space-x-4">
+                                        <div className="w-8 h-8">
+                                            <img className="w-full h-full dark:brightness-75	" alt="logo" src="https://i.ibb.co/L8KSdNQ/image-3.png" />
+                                        </div>
+                                        <div className="flex flex-col justify-start items-center">
+                                            <p className="text-lg leading-6 font-semibold Text-black">
+                                                DPD Delivery
+                                                <br />
+                                                <span className="font-normal">Delivery with 24 Hours</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <p className="text-lg font-semibold leading-6 Text-black">$8.00</p>
+                                </div>
+                                <div className="w-full flex justify-center items-center">
+                                    <button className="hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-5 w-96 md:w-full bg-gray-800 dark:bg-gray-600 text-base font-medium leading-4 text-white dark:text-gray-400">View Carrier Details</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-50 dark:bg-gray-800 w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col ">
+                        <h3 className="text-xl font-semibold leading-5 Text-black">Customer</h3>
+                        <div className="flex  flex-col md:flex-row xl:flex-col justify-start items-stretch h-full w-full md:space-x-6 lg:space-x-8 xl:space-x-0 ">
+                            <div className="flex flex-col justify-start items-start flex-shrink-0">
+                                <div className="flex justify-center  w-full  md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
+                                    <img src="https://i.ibb.co/5TSg7f6/Rectangle-18.png" alt="avatar" />
+                                    <div className=" flex justify-start items-start flex-col space-y-2">
+                                        <p className="text-base font-semibold leading-4 text-left Text-black">David Kent</p>
+                                        <p className="text-sm leading-5 text-gray-600">10 Previous Orders</p>
+                                    </div>
+                                </div>
+
+                                <div className="flex justify-center  md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5Z" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
+                                        <path d="M3 7L12 13L21 7" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
+                                    </svg>
+                                    <p className="cursor-pointer text-sm leading-5 Text-black">david89@gmail.com</p>
+                                </div>
+                            </div>
+                            <div className="flex justify-between xl:h-full  items-stretch w-full flex-col mt-6 md:mt-0">
+                                <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row  items-center md:items-start ">
+                                    <div className="flex justify-center md:justify-start  items-center md:items-start flex-col space-y-4 xl:mt-8">
+                                        <p className="text-base font-semibold leading-4 text-center md:text-left Text-black">Shipping Address</p>
+                                        <p className="w-48 lg:w-full xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">180 North King Street, Northhampton MA 1060</p>
+                                    </div>
+                                    <div className="flex justify-center md:justify-start  items-center md:items-start flex-col space-y-4 ">
+                                        <p className="text-base font-semibold leading-4 text-center md:text-left Text-black">Billing Address</p>
+                                        <p className="w-48 lg:w-full xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">180 North King Street, Northhampton MA 1060</p>
+                                    </div>
+                                </div>
+                                <div className="flex w-full justify-center items-center md:justify-start md:items-start">
+                                    <button className="mt-6 md:mt-0 py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 dark:bg-gray-600  font-medium w-96 2xl:w-full text-base leading-4 Text-black">Edit Details</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Order
---
+++ pages/contact.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import Layout from "../components/user_layout/Layout"
+const contact = () => {
+   return (
+      <Layout>
+         <section className="text-gray-800 dark:text-gray-100 bg-gray-100 dark:bg-gray-700 py-20 lg:py-[120px] overflow-hidden z-10">
+            <div className="container mx-auto px-10">
+               <div className="flex mx-auto flex-wrap lg:justify-between ">
+                  <div className="w-full lg:w-1/2 xl:w-6/12 px-4">
+                     <div className="max-w-[570px] mb-12 lg:mb-0">
+                        <span className="block mb-4 text-base text-primary font-semibold">
+                           Contact Us
+                        </span>
+                        <h2
+                           className="text-dark mb-6 uppercase font-bold text-[32px] sm:text-[40px] lg:text-[36px] xl:text-[40px]">
+                  
+                           GET IN TOUCH WITH US
+                        </h2>
+                        <p className=" text-gray-800 dark:text-gray-100 leading-relaxed mb-9">
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                           eius tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                           adiqua minim veniam quis nostrud exercitation ullamco
+                        </p>
+
+                     </div>
+                  </div>
+                  <div className="w-full lg:w-1/2 xl:w-5/12 px-4">
+                     <div className="dark:text-gray-100 text-gray-200 bg-gray-200 dark:bg-gray-600 relative rounded-lg p-8 sm:p-12 shadow-lg">
+                        <form>
+                           <div className="mb-6">
+                              <input
+                                 type="text"
+                                 placeholder="Your Name"
+                                 className="w-full rounded py-3 px-[14px] dark:bg-gray-700 border border-[f0f0f0] outline-none focus-visible:shadow-none focus:border-primary"
+                              />
+                           </div>
+                           <div className="mb-6">
+                              <input
+                                 type="email"
+                                 placeholder="Your Email"
+                                 className="w-full rounded py-3 px-[14px] dark:bg-gray-700 border border-[f0f0f0] outline-none focus-visible:shadow-none focus:border-primary "
+                        
+                              />
+                           </div>
+                           <div className="mb-6">
+                              <input
+                                 type="text"
+                                 placeholder="Your Phone"
+                                 className="w-full rounded py-3 px-[14px] dark:bg-gray-700 border border-[f0f0f0] outline-none focus-visible:shadow-none focus:border-primary " />
+                        
+                           </div>
+                           <div className="mb-6">
+                              <textarea
+                                 rows="6"
+                                 placeholder="Your Message"
+                                 className="w-full rounded py-3 px-[14px] dark:bg-gray-700 border border-[f0f0f0] resize-none outline-none focus-visible:shadow-none focus:border-primary "
+                        
+                              ></textarea>
+                           </div>
+                           <div>
+                              <button type="submit"  className="w-full text-white bg-indigo-600 dark:bg-indigo-800 rounded border border-primary p-3 transition hover:bg-opacity-90">
+                                 Send Message
+                              </button>
+                           </div>
+                        </form>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </Layout>
+   )
+}
+
+export default contact
